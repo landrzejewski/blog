d031b1cdc6261468f3b78c171d3039a56f924a08
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2026-07-14 14:38:13 +0200

    update

---
+++ src/main/java/pl/training/blog/adapters/persistence/ArticleEntity.java
@@ -0,0 +1,66 @@
+package pl.training.blog.adapters.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity(name = "Article")
+public class ArticleEntity {
+
+    @Id
+    private UUID id;
+    @Column(length = 100)
+    private String title;
+    @Lob
+    private String content;
+    private boolean isDraft;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isDraft() {
+        return isDraft;
+    }
+
+    public void setDraft(boolean draft) {
+        isDraft = draft;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ArticleEntity that = (ArticleEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
