237cb10919f714aa4cc1608c58c186a69d9680c9
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2026-07-14 15:34:35 +0200

    update

---
+++ src/main/java/pl/training/blog/adapters/persistence/JpaArticleRepository.java
@@ -0,0 +1,14 @@
+package pl.training.blog.adapters.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface JpaArticleRepository extends JpaRepository<ArticleEntity, UUID> {
+
+    @Query("select a from Article a where a.isDraft = false")
+    List<ArticleEntity> findAllDrafts();
+
+}
